de127f1f14cfcd0baf112f35064a6e3c2bd9760e
Chloe Brooks <69806061+ccbrooks@users.noreply.github.com>
2023-03-29 18:13:53 -0400

    Create Loading Books Data

---
+++ loading-book-data.sql
@@ -0,0 +1,17 @@
+-- create a new database
+CREATE DATABASE book_shop;
+USE book_shop;
+
+-- in Workbench
+	-- run the query
+
+-- table books
+SELECT * FROM books;
+DESC books;
+
+-- in Command Line Client
+DROP TABLE books;
+
+-- how to run book_data.sql
+-- must be done in a database, USE a database first
+-- source book_data.sql;
